03225456faf3f914f52fcb022e096242e2d8c9eb
VerneZhong <verne.zhong@gmail.com>
2025-06-10 21:54:31 +0900

    增加密码加密存储

--- GitStatHub-Backend/src/main/java/com/syou/gitstathub/service/AuthService.java
+++ GitStatHub-Backend/src/main/java/com/syou/gitstathub/service/AuthService.java
@@ -4,7 +4,9 @@ import com.syou.gitstathub.model.User;
 import com.syou.gitstathub.repository.UserRepository;
 import com.syou.gitstathub.request.RegisterRequest;
 import com.syou.gitstathub.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
@@ -18,15 +20,18 @@ import java.util.Comparator;
 public class AuthService {
     private final UserRepository userRepository;
 
-    public AuthService(UserRepository userRepository) {
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public String login(String username, String password) {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() ->
                         new UsernameNotFoundException("ユーザーが見つかりません：" + username));
-        if (user == null || !user.getPassword().equals(password)) {
+        if (user == null || !passwordEncoder.matches(password, user.getPassword())) {
             throw new RuntimeException("Invalid credentials");
         }
         return JwtUtil.generateToken(user.getUsername());
@@ -35,7 +40,7 @@ public class AuthService {
     public void register(RegisterRequest registerRequest) {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
         user.setAccountNonExpired(1);
         user.setAccountNonLocked(1);
