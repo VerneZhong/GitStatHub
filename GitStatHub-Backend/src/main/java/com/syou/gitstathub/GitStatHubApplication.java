4f0d16810c80f4084e8355f16139e782ef50948c
VerneZhong <verne.zhong@gmail.com>
2025-06-19 22:04:15 +0900

    代码优化

--- GitStatHub-Backend/src/main/java/com/syou/gitstathub/GitStatHubApplication.java
+++ GitStatHub-Backend/src/main/java/com/syou/gitstathub/GitStatHubApplication.java
@@ -18,6 +18,9 @@ public class GitStatHubApplication {
         // 注册到系统环境变量，供 Spring 使用
         System.setProperty("GITHUB_TOKEN", dotenv.get("GITHUB_TOKEN"));
         System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         SpringApplication.run(GitStatHubApplication.class, args);
     }
 }
