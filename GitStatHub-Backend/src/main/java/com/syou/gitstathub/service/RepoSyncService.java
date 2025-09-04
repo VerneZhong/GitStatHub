a343deaa51add87a87631112339730df0642c0e7
VerneZhong <verne.zhong@gmail.com>
2025-09-04 20:56:12 +0900

    去除无效代码

--- GitStatHub-Backend/src/main/java/com/syou/gitstathub/service/RepoSyncService.java
+++ GitStatHub-Backend/src/main/java/com/syou/gitstathub/service/RepoSyncService.java
@@ -5,7 +5,6 @@ import com.syou.gitstathub.repository.RepoInfoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
